f3668015fb32ea8558a6f713faf6c61bcedc3ec7
TheShadowsSpeak <69861459+TheShadowsSpeak@users.noreply.github.com>
2020-10-24 13:38:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,84 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+
+var packageBody,ground;
+var bottom, right,left;
+var bottombody,rightbody2,leftbody3;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.3
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.9
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color("#006100")
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.7, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+	
+	bottom=createSprite(400,650,200,20);
+	bottom.shapeColor=color("#FF0000");
+    right=createSprite(310,610,20,100);
+	right.shapeColor=color("#FF0000");
+	left=createSprite(500,610,20,100);
+	left.shapeColor=color("#FF0000")
+	bottombody=Bodies.rectangle(400,650,200,10, {isStatic:true});
+	rightbody=Bodies.rectangle(310,610,200,10, {isStatic:true});
+	leftbody=Bodies.rectangle(500,610,20,100, {isStatic:true})
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  bottom.x=bottombody.position.x;
+  bottom.y=bottombody.position.y;
+  right.x=rightbody.position.x;
+  right.y=leftbody.position.y;
+  left.x=leftbody.position.x;
+  left.y=leftbody.position.y;
+  keyPressed();
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on
+   Matter.Body.setStatic(packageBody,false);
+}
+}
+
+
+
+
